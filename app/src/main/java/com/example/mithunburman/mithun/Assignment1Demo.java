0e7418df2220180a28aedf6b4c5593f7bfe7f78e
MithunBurman7 <mithunburman7@gmail.com>
2017-07-04 16:50:51 +0530

    Android Assg 1
    Created App, Printed "Hello world"

---
+++ app/src/main/java/com/example/mithunburman/mithun/Assignment1Demo.java
@@ -0,0 +1,13 @@
+package com.example.mithunburman.mithun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Assignment1Demo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment1_demo);
+    }
+}
